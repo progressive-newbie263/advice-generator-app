792098a7afac9ac5a2d3f71d8e3e58600e017f3e
Quang <159697999+progressive-newbie263@users.noreply.github.com>
2024-09-03 22:54:38 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './styles/App.css'
+import './styles/index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
