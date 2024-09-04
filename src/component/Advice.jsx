bd4345e500b3e91dc5597d3eda749773ce32b229
Quang <159697999+progressive-newbie263@users.noreply.github.com>
2024-09-04 11:35:53 +0700

    fixing svg image error

--- src/component/Advice.jsx
+++ src/component/Advice.jsx
@@ -1,4 +1,7 @@
 import React, {useState, useEffect} from "react";
+import PdDesktop from "./svgComponent/pdDesktop.jsx";
+import Dice from "./svgComponent/Dice.jsx";
+import PdMobile from "./svgComponent/pdMobile.jsx";
 
 function Advice() {
   //useEffect (an empty array) only when the component mounts
@@ -8,7 +11,22 @@ function Advice() {
 
   const [isLoaded, setIsLoaded] = useState(false);
   const [advice, setAdvice] = useState({});
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 600);
+ 
+  //adjusting image from screensize:
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 600);
+    };
 
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
+  //fetching data from API:
   const getAdvice = async () => {
     try {
       const response = await fetch('https://api.adviceslip.com/advice'); //api given by challenge
@@ -43,9 +61,21 @@ function Advice() {
         "{advice.advice}"
       </div>
       
-      <div className="mb-8">
-        <img src="./src/images/pattern-divider-desktop.svg" alt="icon-divider" />
-      </div>
+
+      {/* if screen is larger than 600px return PdDesktop component
+          else return pdMobile component.*/}
+      {isLargeScreen ? (
+        <div className="mb-8">
+        {/* <img src="./src/images/pattern-divider-desktop.svg" alt="icon-divider"/> */}
+          <PdDesktop alt="icon-desktop-divider" />
+        </div>
+      ) : (
+        <div className="mb-8">
+        {/* <img src="./src/images/pattern-divider-desktop.svg" alt="icon-divider"/> */}
+          <PdMobile alt="icon-mobile-divider" />
+        </div>
+      )}
+      
       
       {/*  bg-color: neon green/ 
         hsl: hsl(150, 100%, 66%) or 
@@ -56,12 +86,13 @@ function Advice() {
       */}
       <button className={`bg-[#52ffa8] absolute -bottom-[60px] right-[165px] rounded-full p-4 
       transition-all ease-in-out delay-150 
-      hover:shadow-[0_0_15px_hsla(150,100%,66%,0.5),_0_0_15px_hsla(150,100%,66%,0.5),_0_0_15px_hsl(150,100%,66%,0.5)]
+        hover:shadow-[0_0_15px_hsla(150,100%,66%,0.5),_0_0_15px_hsla(150,100%,66%,0.5),_0_0_15px_hsl(150,100%,66%,0.5)]
       sm:right-[120px]
         ${!isLoaded ? 'loading' : ''}`} 
         onClick={adviceDisplay}
       >
-        <img src="./src/images/icon-dice.svg" alt="icon-submit" />
+        {/* <img src="./src/images/icon-dice.svg" alt="icon-submit" /> */}
+        <Dice alt="icon-submit" />
       </button>
     </div>
   );
---
+++ src/component/svgComponent/PdDesktop.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function PdDesktop() {
+  return (
+    <svg width="w-full" height="16" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fill-rule="evenodd">
+        {/* customized path and g, figured. */}
+        <path transform="translate(-30)" fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/>
+        <g transform="translate(180)" fill="#CEE3E9">
+          <rect width="6" height="16" rx="3"/>
+          <rect x="14" width="6" height="16" rx="3"/>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default PdDesktop;
